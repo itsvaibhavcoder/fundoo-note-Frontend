48fd132b6065e78e41c7e5b20c11c0a47df44aed
Vaibhav <sukalevaibhav53@gmail.com>
2024-09-25 14:01:22 +0530

    added icons for component

--- src/app/components/addnote/addnote.component.ts
+++ src/app/components/addnote/addnote.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { LIST_VIEW_ICON,BRUSH_ICON, TICK_ICON,ARCHIVE_ICON, COLLABRATOR_ICON, COLOR_PALATTE_ICON, DELETE_FOREVER_ICON, IMG_ICON, MORE_ICON, REMINDER_ICON, RESTORE_ICON, UNARCHIVE_ICON,UNDO_ICON,REDO_ICON} from 'src/assets/svg-icons';
 @Component({
   selector: 'app-addnote',
   templateUrl: './addnote.component.html',
@@ -7,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddnoteComponent implements OnInit {
   showTakeNote: boolean = true;
-  constructor() {}
-
+  isExpanded: boolean = true;
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconLiteral('list-view', sanitizer.bypassSecurityTrustHtml(LIST_VIEW_ICON));
+    iconRegistry.addSvgIconLiteral('brush', sanitizer.bypassSecurityTrustHtml(BRUSH_ICON));
+    iconRegistry.addSvgIconLiteral('Image', sanitizer.bypassSecurityTrustHtml(IMG_ICON));
+    iconRegistry.addSvgIconLiteral('tick', sanitizer.bypassSecurityTrustHtml(TICK_ICON));
+    iconRegistry.addSvgIconLiteral('undo-icon', sanitizer.bypassSecurityTrustHtml(UNDO_ICON));
+    iconRegistry.addSvgIconLiteral('redo-icon', sanitizer.bypassSecurityTrustHtml(REDO_ICON));
+  }
+ 
   ngOnInit(): void {
   }
   toggleTakeNote(){
