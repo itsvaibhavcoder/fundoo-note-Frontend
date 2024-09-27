48d468d474e15bf09bcf1990ea36142bb81677ed
Vaibhav <sukalevaibhav53@gmail.com>
2024-09-27 19:41:55 +0530

    modified

--- src/services/http-service/http.service.ts
+++ src/services/http-service/http.service.ts
@@ -5,31 +5,34 @@ import { Injectable } from '@angular/core';
 })
 export class HttpService {
   private baseUrl: string =  "http://localhost:3000/api/v1/";
-  private authHeader = new HttpHeaders({
-    'Accept': "application/json",
-    Authorization: 'bearer '+localStorage.getItem('accessToken') || ""
-  })
+  getHeader(){
+     return new HttpHeaders({
+      'Accept': "application/json",
+      Authorization: 'bearer '+localStorage.getItem('accessToken') || ""
+    })
+  }
+   
   constructor(private http: HttpClient) { }
   
   getApiCall(endPoint:string){
     return this.http.get(this.baseUrl+endPoint, {
-      headers: this.authHeader
+      headers: this.getHeader()
     })
   }
   
   postApiCall(endPoint:string, payload:any, addHeader:boolean=false){
-   return this.http.post(this.baseUrl+endPoint, payload, {headers:addHeader? this.authHeader: {} })
+   return this.http.post(this.baseUrl+endPoint, payload, {headers:addHeader? this.getHeader(): {} })
   }
 
   deleteApiCall(endPoint:string){
     return this.http.delete(this.baseUrl + endPoint,{
-      headers: this.authHeader
+      headers: this.getHeader()
     })
   }
 
   putApiCall(endPoint: string, payload: any) {
     return this.http.put(this.baseUrl + endPoint, payload, {
-      headers: this.authHeader
+      headers: this.getHeader()
     });
   }
 
