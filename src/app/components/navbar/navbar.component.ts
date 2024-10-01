a645cf248ef92e7c594377f970e9d08000245dd9
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-02 01:52:48 +0530

    toggle drawer functionality

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
@@ -10,10 +10,12 @@ import { DataService } from 'src/services/data-service/data.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @Output() toggleDrawer = new EventEmitter<void>();
   searchQuery:string = ""
   showProfile: boolean = false;
   userName: string = localStorage.getItem('name') || ''
   userEmail: string = localStorage.getItem('email') || '';
+
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private dataService: DataService, private router: Router) {
     iconRegistry.addSvgIconLiteral('main-menu', sanitizer.bypassSecurityTrustHtml(MENU_ICON));
     iconRegistry.addSvgIconLiteral('search-icon', sanitizer.bypassSecurityTrustHtml(SEARCH_ICON));
@@ -31,6 +33,10 @@ export class NavbarComponent implements OnInit {
   }
   handleSearchQuery(){
     this.dataService.updateSearchQuery(this.searchQuery)
+  }
+
+  onMenuIconClick(){
+    this.toggleDrawer.emit();
   }
 
 }
