1a23c4a19b9a4eb2caf455c60ee6820093919942
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-01 09:48:19 +0530

    save user information in localStorage

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,39 +9,45 @@ import { UserService } from 'src/services/user-service/user.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  loginForm!:FormGroup
-  submitted: boolean=false
-  constructor(private userService: UserService, private router: Router, private formBuilder: FormBuilder) {}
+  loginForm!: FormGroup;
+  submitted: boolean = false;
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
-}
-get loginControls(){
-  return this.loginForm.controls
-}
+  }
+  get loginControls() {
+    return this.loginForm.controls;
+  }
 
   handleLogin() {
-    this.submitted = !this.submitted
-    const {email, password} = this.loginForm.value
-    if(this.loginForm.valid){
-       this.userService
-      .loginSignUpApiCall('users/login', {
-      email: email,
-      password: password,
-      })
-      .subscribe({
-        next: (res:any) => {
-          console.log(res);
-          localStorage.setItem("accessToken",res?.data?.generatedToken);
-          this.router.navigate(['/notes'])
-        },
-        error: (err) => {
-          console.log(err);
-        },
-      });
+    this.submitted = !this.submitted;
+    const { email, password } = this.loginForm.value;
+    if (this.loginForm.valid) {
+      this.userService
+        .loginSignUpApiCall('users/login', {
+          email: email,
+          password: password,
+        })
+        .subscribe({
+          next: (res: any) => {
+            console.log(res);
+            localStorage.setItem('accessToken', res?.data?.generatedToken);
+            localStorage.setItem('name', res?.data?.firstName);
+            localStorage.setItem('email', res?.data?.email);
+            this.router.navigate(['/notes']);
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
     }
   }
 }
