4ebecb7e75f37a029cc768829f00ccf53bc81556
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-01 09:50:41 +0530

    modified

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { SEARCH_ICON, MENU_ICON,  SETTING_ICON, REFRESH_ICON,LIST_VIEW_ICON } from 'src/assets/svg-icons';
 import { DataService } from 'src/services/data-service/data.service';
 @Component({
@@ -10,7 +11,10 @@ import { DataService } from 'src/services/data-service/data.service';
 })
 export class NavbarComponent implements OnInit {
   searchQuery:string = ""
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private dataService: DataService) {
+  showProfile: boolean = false;
+  userName: string = localStorage.getItem('name') || ''
+  userEmail: string = localStorage.getItem('email') || '';
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private dataService: DataService, private router: Router) {
     iconRegistry.addSvgIconLiteral('main-menu', sanitizer.bypassSecurityTrustHtml(MENU_ICON));
     iconRegistry.addSvgIconLiteral('search-icon', sanitizer.bypassSecurityTrustHtml(SEARCH_ICON));
     iconRegistry.addSvgIconLiteral('refresh-icon', sanitizer.bypassSecurityTrustHtml(REFRESH_ICON));
@@ -21,8 +25,12 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  logout(){
+    localStorage.clear();
+    this.router.navigate(['/'])
+  }
   handleSearchQuery(){
     this.dataService.updateSearchQuery(this.searchQuery)
   }
+
 }
