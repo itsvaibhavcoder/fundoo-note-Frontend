3bda940dc0b9bb97d364e689638eb0c5cc6a41d0
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-02 00:14:22 +0530

    added drawer

--- src/app/components/dashboard-container/dashboard-container.component.ts
+++ src/app/components/dashboard-container/dashboard-container.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { MatDrawer } from '@angular/material/sidenav';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ARCHIVE_ICON, REMINDER_ICON,  EDIT_ICON,TRASH_ICON, BULB_ICON} from 'src/assets/svg-icons';
 @Component({
   selector: 'app-dashboard-container',
   templateUrl: './dashboard-container.component.html',
   styleUrls: ['./dashboard-container.component.scss']
 })
 export class DashboardContainerComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('drawer')drawer!: MatDrawer;
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconLiteral('reminder-icon', sanitizer.bypassSecurityTrustHtml(REMINDER_ICON));
+    iconRegistry.addSvgIconLiteral('archive-icon', sanitizer.bypassSecurityTrustHtml(ARCHIVE_ICON));
+    iconRegistry.addSvgIconLiteral('edit-icon', sanitizer.bypassSecurityTrustHtml(EDIT_ICON));
+    iconRegistry.addSvgIconLiteral('trash-icon', sanitizer.bypassSecurityTrustHtml(TRASH_ICON));
+    iconRegistry.addSvgIconLiteral('bulb-icon', sanitizer.bypassSecurityTrustHtml(BULB_ICON));
+  }
 
   ngOnInit(): void {
   }
-
+  toggleDrawer() {
+    this.drawer.toggle();
+  }
 }
