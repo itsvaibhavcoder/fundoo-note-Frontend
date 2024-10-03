7573cf00e35532b5bd4edb1eed51b7bae61e2227
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-03 12:25:51 +0530

    search functionality

--- src/app/components/archieve-container/archieve-container.component.ts
+++ src/app/components/archieve-container/archieve-container.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DataService } from 'src/services/data-service/data.service';
 import { NotesService } from 'src/services/notes-service/notes.service';
 
 @Component({
@@ -6,13 +8,19 @@ import { NotesService } from 'src/services/notes-service/notes.service';
   templateUrl: './archieve-container.component.html',
   styleUrls: ['./archieve-container.component.scss']
 })
-export class ArchieveContainerComponent implements OnInit {
+export class ArchieveContainerComponent implements OnInit, OnDestroy {
   notesList: any[] = [];
-  
-  constructor(private noteService: NotesService) { }
+  searchQuery: string = '';
+  subscription!: Subscription;
+  constructor(private noteService: NotesService, private dataService: DataService) { }
 
   ngOnInit(): void {
     this.loadNotes();
+    this.dataService.curSearchQuery.subscribe({
+      next: (data) => {
+        this.searchQuery = data;
+      },
+    });
   }
 
   loadNotes() {
@@ -33,5 +41,10 @@ export class ArchieveContainerComponent implements OnInit {
     if ($event.action === 'unarchive') {
       this.notesList = this.notesList.filter(note => note._id !== $event.data._id);
     } 
+  }
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
