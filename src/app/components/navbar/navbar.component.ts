b2d7555eb0cfaf61d90524e5911cb4b2534193c0
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-10 23:46:57 +0530

    fixed horizontal scrollbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Renderer2, OnInit, Output } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
@@ -13,10 +13,11 @@ export class NavbarComponent implements OnInit {
   @Output() toggleDrawer = new EventEmitter<void>();
   searchQuery:string = ""
   showProfile: boolean = false;
+  isDarkTheme: boolean = false;
   userName: string = localStorage.getItem('name') || ''
   userEmail: string = localStorage.getItem('email') || '';
   
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private dataService: DataService, private router: Router) {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private dataService: DataService, private router: Router, private renderer: Renderer2) {
     iconRegistry.addSvgIconLiteral('main-menu', sanitizer.bypassSecurityTrustHtml(MENU_ICON));
     iconRegistry.addSvgIconLiteral('search-icon', sanitizer.bypassSecurityTrustHtml(SEARCH_ICON));
     iconRegistry.addSvgIconLiteral('refresh-icon', sanitizer.bypassSecurityTrustHtml(REFRESH_ICON));
@@ -25,7 +26,25 @@ export class NavbarComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    // const savedTheme = localStorage.getItem('theme');
+    // if(savedTheme){
+    //   this.isDarkTheme = savedTheme==='dark';
+    //   this.loadTheme();
+    // }
   }
+  // changeTheme(){
+  //  this.isDarkTheme = !this.isDarkTheme;
+  //  localStorage.setItem('theme', this.isDarkTheme ? 'dark': 'light');
+  //  this.loadTheme();
+  // }
+
+  // loadTheme(){
+  //   const themeLink = document.getElementById('app-theme') as HTMLLinkElement;
+
+  //   if (this.isDarkTheme) {
+  //     themeLink.href = this.isDarkTheme ? 'assets/theme/dark-theme.scss':'assets/theme/light-theme.scss';
+  //   } 
+  // }
   logout(){
     localStorage.clear();
     this.router.navigate(['/'])
